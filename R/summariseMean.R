c5370f864afefca0279e6844a879bc27b066868c
nmprista <31890653+nmprista@users.noreply.github.com>
2025-10-27 14:31:10 +0100

    fixed bug column selection

--- R/summariseMean.R
+++ R/summariseMean.R
@@ -1,6 +1,6 @@
 summariseMean<-function(x, group=c("lanID","sp")){
 
-if (group!="") cols<-c(group,colnames(x)[grepl(colnames(x), pat="^n_")]) else cols<-c(colnames(x)[grepl(colnames(x), pat="^n_")])
+if (!is.null(group)) cols<-c(group,colnames(x)[grepl(colnames(x), pat="^n_")]) else cols<-c(colnames(x)[grepl(colnames(x), pat="^n_")])
 res<-unique(x[sppWeight_obs>0,..cols])
 res[, lapply(.SD, mean), .SDcols=colnames(x)[grepl(colnames(x), pat="^n_")], by=group]
 
--- R/summariseQuantiles.R
+++ R/summariseQuantiles.R
@@ -6,7 +6,7 @@ apply_func_and_get_names <- function(DT, func, ...) {
    c(list(op = names(res[[1]])), res)
 }
 
-if (group!="") cols<-c(group,colnames(x)[grepl(colnames(x), pat="^n_")]) else cols<-c(colnames(x)[grepl(colnames(x), pat="^n_")])
+if (!is.null(group)) cols<-c(group,colnames(x)[grepl(colnames(x), pat="^n_")]) else cols<-c(colnames(x)[grepl(colnames(x), pat="^n_")])
 res<-unique(dat[sppWeight_obs>0,..cols])
 res[, apply_func_and_get_names(.SD, quantile, probs), .SDcols=colnames(x)[grepl(colnames(x), pat="^n_")], by=group]
 

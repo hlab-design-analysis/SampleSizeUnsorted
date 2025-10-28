e5dfc06a83f696f91908d9b255cc5cd27f5b255c
nmprista <31890653+nmprista@users.noreply.github.com>
2025-10-29 00:13:02 +0100

    fixed bug summariseQuantiles
    was failing in absolute ns NAs (happens when totWeight_obs is NA)
    
    Co-Authored-By: Annica dG <71251688+annicadg@users.noreply.github.com>
    #12

--- R/summariseQuantiles.R
+++ R/summariseQuantiles.R
@@ -5,9 +5,12 @@ apply_func_and_get_names <- function(DT, func, ...) {
    res <- lapply(DT, func, ...)
    c(list(op = names(res[[1]])), res)
 }
+# filters out n cols with NAs (note: they appear when totWeight_obs has NAs)
+tmp<-apply(data.frame(x)[,colnames(x)[grepl(colnames(x), pat="^n_")]],2,sum)
+n_cols<-names(tmp)[!is.na(tmp)]
 
-if (!is.null(group)) cols<-c(group,colnames(x)[grepl(colnames(x), pat="^n_")]) else cols<-c(colnames(x)[grepl(colnames(x), pat="^n_")])
-res<-unique(dat[sppWeight_obs>0,..cols])
-res[, apply_func_and_get_names(.SD, quantile, probs), .SDcols=colnames(x)[grepl(colnames(x), pat="^n_")], by=group]
+if (!is.null(group)) cols<-c(group,n_cols) else cols<-n_cols
+res<-unique(x[,..cols])
+res[, apply_func_and_get_names(.SD, quantile, probs), .SDcols=cols[grepl(cols, pat="^n_")], by=group]
 
 }

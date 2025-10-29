53871d6221418e36cae55d2fcba41aecfcd19c18
nmprista <31890653+nmprista@users.noreply.github.com>
2025-10-29 23:55:39 +0100

    Update estimSppComp.r
    added min_2 #16
    tidy up

--- estimSppComp.r
+++ estimSppComp.r
@@ -22,37 +22,35 @@ dat <- readRDS("data/SPE.rds")
 doInitialChecks(dat)
 
 # estimates weight Composition in Percentage and Absolute Weight
-	# note: aboslute estimates are only calculated if totWeight_obs is provided 
+	# note: absolute estimates are only calculated if totWeight_obs is provided 
 dat <- estimateWeightComp (x = dat,round_Nbuc_estim=FALSE, finitePopCorr=FALSE)
 
 #===============================================
 # calculate sample size for diferent margins of error in proportion
 #===============================================
 
-
-		doSampleSizeGivenError(x=dat, e=c(0.03,0.05,0.07,0.10), error_type="Percent")
+doSampleSizeGivenError(x=dat, e=c(0.03,0.05,0.07,0.10), error_type="Percent")
 		
-		# example
-		unique(dat[nbuc_obs>1,c("lanID","sp","totWeight_obs", "nbuc_obs","bucWeightmean_obs","sppPercWeight_s2","sppPercWeight_estim","n_003","n_005","n_007","n_01")])
+# example
+unique(dat[nbuc_obs>1,c("lanID","sp","totWeight_obs", "nbuc_obs","bucWeightmean_obs","sppPercWeight_s2","sppPercWeight_estim","n_003","n_005","n_007","n_01")])
 
 #===============================================
 # calculate sample size for diferent margins of error in absolute weight
 #===============================================
 
-	doSampleSizeGivenError(x=dat, e=c(1000,500,100), error_type="Absolute")
+doSampleSizeGivenError(x=dat, e=c(1000,500,100), error_type="Absolute")
 		
-		
-		# example
-		unique(dat[nbuc_obs>1,c("lanID","sp","totWeight_obs","nbuc_obs","sppPercWeight_estim","n_01","n_007","n_005","n_003", "n_1000","n_500","n_100")])
+# example
+unique(dat[nbuc_obs>1,c("lanID","sp","totWeight_obs","nbuc_obs","sppPercWeight_estim","n_01","n_007","n_005","n_003", "n_1000","n_500","n_100")])
 
 
 #===============================================
 # summarise results
 #===============================================
 
-summariseMean(x = dat, group="sp")
-summariseMedian(x = dat, group="sp")
-summariseQuantiles(x = dat, group="sp", probs=c(0.025,0.975))
+summariseMean(x = dat, group="sp", min_n=2)
+summariseMedian(x = dat, group="sp", min_n=2)
+summariseQuantiles(x = dat, group="sp", probs=c(0.025,0.975), min_n=2)
 
 #===============================================
 # save results
@@ -61,6 +59,7 @@ summariseQuantiles(x = dat, group="sp", probs=c(0.025,0.975))
 dir.create("results", showWarnings=FALSE)
 # uncomment and tune the next line of code below to save results
 # write.csv(summariseMean(x = dat, group="sp"), file="results/results.csv", row.names=FALSE)
+
 
 
 

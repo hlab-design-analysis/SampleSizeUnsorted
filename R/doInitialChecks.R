a1f903996cf131c9f36e8bfbb2015f5d5e7c13bb
nmprista <31890653+nmprista@users.noreply.github.com>
2025-10-29 22:14:37 +0100

    Update doInitialChecks.R
    added warning and instructions when some totWeight_obs are declared and some missing
    
    #20

--- R/doInitialChecks.R
+++ R/doInitialChecks.R
@@ -27,6 +27,19 @@ if(all(
 # test: only one row per lan, buc, and species
 	all(dat[,.N, .(lanID,bucID,sp)]$N==1) &
 # test: only 1 totWeight_obs per landing
-	all(dat[,.N, .(lanID,totWeight_obs)][,.N, lanID]$N==1) == TRUE
-	)) print ("dat ok") else stop("check consistency of dat object")	
+	all(dat[,.N, .(lanID,totWeight_obs)][,.N, lanID]$N==1) == TRUE 
+
+	)) cat ("dat ok \n\n") else stop("check consistency of dat object")	
+
+# warning: data contain a mix of declared and mixed totWeight_obs
+	# present algorithm cannot fully handle these two cases at the same time
+		# see #
+	if(nrow(dat[,.N,is.na(totWeight_obs)])>1) 
+cat("===Attention=== \n
+The data contains some totWeight_obs missing and others declared: \n
+The analyses should be split in two: \n
+   dat<-dat[!is.na(totWeight_obs),] # where both % and absolute weights can be calculated \n
+   dat<-dat[is.na(totWeight_obs),] # where only % can be calculated\n
+with results 'rbind-ed' in the end \n \n")
+
 }

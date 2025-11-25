209d04a7948533fe67eb302995c8104415b97693
nmprista <31890653+nmprista@users.noreply.github.com>
2025-11-25 21:49:14 +0100

    Create summariseMax.R
    adding #25

---
+++ R/summariseMax.R
@@ -0,0 +1,56 @@
+#' Max of sample size estimates
+#'
+#' This function calculates the max of sample size estimates obtained for a set of landings.
+#'
+#' @param x dataset containing estimated sample size columns ("n_*" columns).
+#' @param group a vector of column names defining how to group results. Default "sp" provides 
+#' max sample size by error margin and species)
+#' @param min_n integer defining the minimum sample size accepted. Default is 2. See details. 
+#'
+#' @return data.table with means by group
+#'
+#' @details A minimum of 2 samples is needed to calculate a variance. If group == NULL returns
+#' the largest sample size of species across all landings. If group == "lanID" returns
+#' the largest sample size of species in each landing. If the dataset contains
+#' extra variable, e.g., a fishery column, setting group=c("sp","fishery") will provide the 
+#' mean sample size needed to attain each error margin by sp and fishery.
+#'
+#' @examples
+#' \dontrun{
+#' dat <- readRDS("data/SPE.rds")
+#' doInitialChecks(dat)
+#' dat <- estimateWeightComp(dat)
+#' doSampleSizeGivenError(x=dat, e=c(0.03,0.05,0.07,0.10), error_type="Percent")
+#' summariseMax(x = dat, group="sp")
+#' # compare with
+#' summariseMax(x = dat, group="sp", min_n=0)
+#' }
+
+summariseMax<-function(x, group=c("sp"), min_n=2){
+
+#excludes from calculations landings with only 1 bucket
+if(any(x$nbuc_obs==1)) 
+{
+cat("\n")
+print(paste0("ATT: excluding ",nrow(x[nbuc_obs==1,.N,lanID])," landings with only n=1 buckets sampled: variance is not defined in those cases"))
+x<-x[nbuc_obs>1,]
+cat("\n")
+}
+# filters columns
+if (!is.null(group)) cols<-c("lanID","sp",group,colnames(x)[grepl(colnames(x), pat="^n_")]) else cols<-c("lanID","sp",colnames(x)[grepl(colnames(x), pat="^n_")])
+res<-unique(x[nbuc_obs>1,..cols])
+# sets the minimum sample size
+if(!min_n==0) {
+aux<-colnames(res)[grepl(colnames(res), pat="^n_")]
+res<-cbind(res[,..group],res[, lapply(.SD, function(x) ifelse(x %in% c(0:min_n-1),min_n,x)),.SDcols=aux])
+}
+# calculates the max
+out<-res[, lapply(.SD, max), .SDcols=colnames(x)[grepl(colnames(x), pat="^n_")], by=group]
+if (!is.null(group)) {
+cols2<-c(group, "lanID")
+out<-merge(x[nbuc_obs>1,.N,by=cols2][,.N,by=group],out)
+} else {
+out
+}
+out
+}

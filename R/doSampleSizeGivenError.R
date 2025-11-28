9675f01cef61b4286d334eb6ddf755038eb0e7bb
nmprista <31890653+nmprista@users.noreply.github.com>
2025-11-28 16:55:41 +0100

    improve format column names
    even better solution for the minor problem

--- R/doSampleSizeGivenError.R
+++ R/doSampleSizeGivenError.R
@@ -4,11 +4,12 @@
 		if(!error_type %in% c("Percent","Absolute")) stop ("error_type value must be either 'Percent' or 'Absolute'")
 		if(!is.numeric(e)) stop ("e value must be numeric")
 		if(! all(c("lanID", "sp", "totWeight_obs","bucWeightmean_obs", "sppPercWeight_s2") %in% colnames(x))) stop ("check colnames in x")
-
+		
+		sprintfnumber<-max(nchar(c(0.02,0.025,0.05,0.07,0.10)))-2
 
 		if (error_type=="Percent")
 			{
-			for (i in 1:length(e)) x[, paste0("n_", gsub("\\.","",sprintf("%0.3f",e[i]))):=ceiling(1/bucWeightmean_obs^2*(1.96*sqrt(sppPercWeight_s2)/e[i])^2), by=.(lanID, sp)]
+			for (i in 1:length(e)) x[, paste0("n_", sprintf(paste0("%0.",sprintfnumber,"f"),e[i])):=ceiling(1/bucWeightmean_obs^2*(1.96*sqrt(sppPercWeight_s2)/e[i])^2), by=.(lanID, sp)]
 			}
 		if (error_type=="Absolute")
 			{

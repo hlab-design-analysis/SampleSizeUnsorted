a287ba5f82c52c565ec453d15f2b58a4eb4a659b
nmprista <31890653+nmprista@users.noreply.github.com>
2026-02-17 06:03:47 +0100

    Update plotsAndAnalyses.r
    added a bunch of new plots and analysis (done for meeting18/01/2026)

--- plotsAndAnalyses.r
+++ plotsAndAnalyses.r
@@ -1,13 +1,76 @@
 #source("estimSppComp.r")
 
 #===============================================
-# displays the more variable cases within each fishery
+# create directories
 #===============================================
 
-	min_number_lanIDs<-10
+for (i in unique(dat$ctry))
+{
+for (j in unique(dat$fisheryArea))
+{
+	graph_dir<-paste0("results/",target_country,"/plots_individual_landings/",format(Sys.Date(), format="%Y%m%d"),"/country/",i,"/",j,"/")
+	dir.create(graph_dir, recursive=T, showWarnings=F)
+}
+}
+for (i in unique(dat$fisheryArea))
+{
+	graph_dir<-paste0("results/",target_country,"/plots_individual_landings/",format(Sys.Date(), format="%Y%m%d"),"/fishery/",i,"/")
+	dir.create(graph_dir, recursive=T, showWarnings=F)
+}
+
+#===============================================
+# graphs of individual landings
+#===============================================
+	# spp composition in each landing bucket by bucket
+	# saves files organized by country/fishery AND fishery (files repeated)
+
+	base_graph_dir <- paste0("results/",target_country,"/plots_individual_landings/",format(Sys.Date(), format="%Y%m%d"))
+
+	ls1<-split(dat, dat$fisheryArea)
+	for (i in names(ls1))
+		{
+		print(i)
+		tmp_fisheryArea <- ls1[[i]]
+		tmp_fisheryArea$colour<-as.integer(factor(tmp_fisheryArea$sp))
+		for (j in unique(tmp_fisheryArea$lanID))
+		{
+		print(paste0(".",j,": ",which(unique(tmp_fisheryArea$lanID)==j)," out of ",length(unique(tmp_fisheryArea$lanID))))
+		tmp_trip<-tmp_fisheryArea[lanID==j,]
+		dat_graph<-tmp_trip[sppPercWeight_estim>0,sppWeight_obs/bucWeight_obs, by=.(lanID, bucID,sp, totWeight_obs, sppPercWeight_estim, n_0.050, colour)]
+		dat_graph2<-tmp_trip[sppPercWeight_estim>0,.N,.(lanID, sp, totWeight_obs, sppPercWeight_estim, sppPercWeight_estim_CIlow, sppPercWeight_estim_CIupp, sppWeight_estim, sppWeight_estim_CIlow, sppWeight_estim_CIupp,colour)]
+		dat_graph<-dat_graph[order(bucID),]
+		dat_graph$bucOrder<-as.integer(factor(dat_graph$bucID))
+		png(filename = paste0(base_graph_dir,"/fishery/",i,"/",i,"_",j,".png"),width = 960, height = 480)
+		par(oma=c(1,1,1,5))
+		if(max(dat_graph$bucOrder)<50) plot(V1~bucOrder, data=dat_graph, type="n", ylim=c(0,1), xlim=c(0,50), pch=19, main="", xlab="bucOrder", ylab="proportion in weight")
+		if(max(dat_graph$bucOrder)>=50) plot(V1~bucOrder, data=dat_graph, type="n", ylim=c(0,1), xlim=c(0,100), pch=19, main="", xlab="bucOrder", ylab="proportion in weight")
+		title(main=paste0(i,": ",j,":\n total weight:",round(dat_graph$totWeight_obs[1]/1000), " ton\n","n_0.050: ", max(tmp_trip[sppWeight_obs>0,]$n_0.050),"; n_10000: ", max(tmp_trip[sppWeight_obs>0,]$n_10000)), cex.main=0.8)
+		for (w in unique(dat_graph$sp))
+		{	
+		points(V1~bucOrder, data=dat_graph[sp==w,], type="o", lty=2, pch=19, col=unique(dat_graph[sp==w,]$colour))
+		text_to_print<-paste0(w,": ",round(mean(dat_graph2[sp==w,]$sppPercWeight_estim),2),"(",round(mean(dat_graph2[sp==w,]$sppPercWeight_estim_CIlow),2),"-",round(mean(dat_graph2[sp==w,]$sppPercWeight_estim_CIupp),2),")")
+		text(x=max(dat_graph[sp==w,]$bucOrder), y=mean(dat_graph[sp==w,]$sppPercWeight_estim), labels=text_to_print, col=unique(dat_graph[sp==w,]$colour), cex=0.7, pos=4)
+		# margin 4 text: total weight estimates
+			mtext("Westim (t); IC 95%", side=4, at=1.05, line=0.5, las=2, cex=0.8)
+			text_to_print<-paste0(w,": ",round(dat_graph2[sp==w,]$sppWeight_estim/1000), "t: ", round(dat_graph2[sp==w,]$sppWeight_estim_CIlow/1000),"-",round(dat_graph2[sp==w,]$sppWeight_estim_CIupp/1000))
+			mtext(text_to_print, side=4, at=dat_graph2[sp==w,]$sppPercWeight_estim, las=1, col=dat_graph2[sp==w,]$colour, line=0.5, cex=0.7)
+		}
+		# copies also to country directory
+		file.copy(from=paste0(base_graph_dir,"/fishery/",i,"/",i,"_",j,".png"), to = paste0(base_graph_dir,"/country/", substr(j,1,3),"/",i,"/",i,"_",j,".png"))
+		dev.off()
+}
+}
+
+
+#===============================================
+# displays the more variable cases within each fishery
+#===============================================
+	
+	number_of_cases_to_display<-8
+	min_number_lanIDs<-15
 	target_fisheryAreas<-summary_095_min_bucs_obs_5[nLanIDs>=min_number_lanIDs]$fisheryArea
 	ls1<-split(dat[fisheryArea %in% target_fisheryAreas,], dat[fisheryArea %in% target_fisheryAreas,]$fisheryArea)
-	a<-lapply(ls1,function(x){print(head(unique(x[nbuc_obs>=5,.(fisheryArea, lanID, sp, n_0.050)])[order(-n_0.050),],8))})
+	a<-lapply(ls1,function(x){print(head(unique(x[nbuc_obs>=5,.(fisheryArea, lanID, nbuc_obs, sp, n_0.050, sppWeight_estim, sppWeight_estim_CIlow, sppWeight_estim_CIupp)])[order(-n_0.050),],number_of_cases_to_display))})
 
 
 #===============================================
@@ -16,6 +79,14 @@
 
 # To be done
 
+# position of the max in the ordered vector
+lapply(split(summariseMax(x = dat[nbuc_obs>=min_bucs_obs,], group=c("lanID","fisheryArea"), min_n=2),by="fisheryArea"), function(x) { browser()
+pos <- which(unname(unlist(x[order(x[,"n_0.050"]),][,"n_0.050"]))==round(apply(x[order(x[,"n_0.050"])][,"n_0.050"],2,quantile, type=7, prob=c(0.95)))); pos
+if(length(pos)==0) pos <- max(which(unname(unlist(x[order(x[,"n_0.050"]),][,"n_0.050"]))<round(apply(x[order(x[,"n_0.050"])][,"n_0.050"],2,quantile, type=7, prob=c(0.95))))); pos
+#if(length(pos)>1) print("many cases: ambiguous")
+x[order(x[,"n_0.050"]),][pos,]$lanID
+})
+
 #===============================================
 # displays the more variable cases in each country
 #===============================================
@@ -23,37 +94,7 @@
 	ls1<-split(dat, dat$ctry)
 	a<-lapply(ls1,function(x){print(head(unique(x[nbuc_obs>=5,.(ctry, fisheryArea, lanID, sp, n_0.050)])[order(-n_0.050),]))})
 
-#===============================================
-# graphs of individual landings
-#===============================================
 
-	graph_dir_country<-paste0("results/",target_country,"/plots_individual_landings/")
-	if(!dir.exists(graph_dir_country)) dir.create(graph_dir_country, recursive=T)
-
-	ls1<-split(dat, dat$fisheryArea)
-	for (i in names(ls1)[7])
-		{
-		print(i)
-		tmp_fisheryArea <- ls1[[i]]
-		tmp_fisheryArea$colour<-as.integer(factor(tmp_fisheryArea$sp))
-		for (j in unique(tmp_fisheryArea$lanID))
-		{
-		#print(j)
-		tmp_trip<-tmp_fisheryArea[lanID==j,]
-		dat_graph<-tmp_trip[,sppWeight_obs/bucWeight_obs, by=.(lanID, bucID,sp, totWeight_obs, sppPercWeight_estim, n_0.050, colour)]
-		dat_graph<-dat_graph[order(bucID),]
-		dat_graph$bucOrder<-as.integer(factor(dat_graph$bucID))
-		png(filename = paste0(graph_dir_country,i,"_",j,".png"),width = 560, height = 480,)
-		plot(V1~bucOrder, data=dat_graph, type="n", ylim=c(0,1), xlim=c(0,50), pch=19, main=paste0(target_country,":", i,": ",j,": ",dat_graph$totWeight_obs[1], " ton\n","n_0.050: ", max(tmp_trip[sppWeight_obs>0,]$n_0.050)), xlab="bucOrder")
-		for (w in unique(dat_graph$sp))
-		{	
-		points(V1~bucOrder, data=dat_graph[sp==w,], type="o", lty=2, pch=19, col=unique(dat_graph[sp==w,]$colour))
-		text(x=max(dat_graph[sp==w,]$bucOrder), y=mean(dat_graph[sp==w,]$sppPercWeight_estim), labels=paste(w,round(mean(dat_graph[sp==w,]$sppPercWeight_estim),2)), col=unique(dat_graph[sp==w,]$colour), cex=0.7, pos=4)
-		}
-		dev.off()
-		#savePlot(paste0(graph_dir_country,i,"_",j,".png"), type="png")
-}
-}
 
 # ===================================
 # histogram: sample sizes required for 95% of trips
@@ -62,10 +103,11 @@
 graph_dir_country<-paste0("results/",target_country,"/plots_histogram_sample_sizes/")
 if(!dir.exists(graph_dir_country)) dir.create(graph_dir_country, recursive=T)
 
-length(subset_fisheryArea<-summary_095_min_bucs_obs_5[meets_min_n_landings==T,]$fisheryArea)
+length(subset_fisheryArea<-summary_095_min_bucs_obs_5[nLanIDs>=14,]$fisheryArea)
 
 for (i in unique(subset_fisheryArea))
 {
+windows()
 hist(summariseMax(x = dat, group=c("lanID","fisheryArea"), min_n=5)[fisheryArea==i,]$n_0.050, breaks=100, main=paste0(i,": samp size for 0.05 e: ",summary_095_min_bucs_obs_5[fisheryArea==i,]$nLanIDs," lanIDs"), xlab="sample size")
 abline(v=summary_095_min_bucs_obs_5[fisheryArea==i,]$n_0.05, lty=2, col="orange")
 abline(v=summary_090_min_bucs_obs_5[fisheryArea==i,]$n_0.05, lty=2, col="red")
@@ -227,12 +269,12 @@ abline(0,0.25)
 #===============================================
 
 # analysis sppPercWeight_estim_cv | sppWeight_estim_cv>5
-dat[sppWeight_estim>0  & !is.na(sppPercWeight_estim_cv) & nbuc_obs>=5, .(lanID,sp, fisheryArea, sppPercWeight_estim_cv)][,list(sum(sppPercWeight_estim_cv>0.05)/.N), by=fisheryArea]
-dat[sppWeight_estim>0  & !is.na(sppWeight_estim_cv) & nbuc_obs>=5, .(lanID,sp, fisheryArea, sppWeight_estim_cv)][,list(sum(sppWeight_estim_cv>0.05)/.N), by=fisheryArea]
+dat[sppWeight_estim>0  & !is.na(sppPercWeight_estim_cv) & nbuc_obs>=5, .(lanID,sp, fisheryArea, sppPercWeight_estim_cv)][,list(sum(sppPercWeight_estim_cv>5)/.N), by=fisheryArea]
+dat[sppWeight_estim>0  & !is.na(sppWeight_estim_cv) & nbuc_obs>=5, .(lanID,sp, fisheryArea, sppWeight_estim_cv)][,list(sum(sppWeight_estim_cv>5)/.N), by=fisheryArea]
 
 
-dat1[sppWeight_estim>0  & !is.na(sppWeight_estim_cv), .(lanID,sp,pred_em_perc, fisheryArea)][,list(sum(pred_em_perc>10)/.N), by=fisheryArea]
 
+dat[sppWeight_estim>0  & !is.na(sppWeight_estim_cv) & nbuc_obs>=5 & sp=="HER", .N, .(lanID,sp, fisheryArea, sppWeight_estim_cv)][,list(sum(sppWeight_estim_cv>5)/.N), by=fisheryArea]
 
 
 	
@@ -245,15 +287,15 @@ proposed_sample_size0<-summary_095_min_bucs_obs_5[nLanIDs>10,.(fisheryArea,n_0.0
 
 proposed_sample_size1<-data.table(fisheryArea=c('Baltic_HERSPR_HUC','Baltic_HERSPR_IND','Bothnia_FVE','Bothnia_HER','NAtlantic_MAC','NAtlantic_WHB','NSea_HER','NSea_NOP','NSea_SAN','NSea_SPR'),
 								 n_0.050=c(Baltic_HERSPR_HUC =  24,
-								 Baltic_HERSPR_IND = 25 ,
+								 Baltic_HERSPR_IND = 30 ,
 								 Bothnia_FVE =  20,
 								 Bothnia_HER =  6,
-								 NAtlantic_MAC =  55 ,
+								 NAtlantic_MAC =  9 ,
 								 NAtlantic_WHB =  1,
-								 NSea_HER =  5,
-								 NSea_NOP =  22,
+								 NSea_HER =  3,
+								 NSea_NOP =  18,
 								 NSea_SAN =  2,
-								 NSea_SPR =  33) #21 alloc; 33 as species; 21 deleted; 12 expert
+								 NSea_SPR =  12) #21 alloc; 33 as species; 21 deleted; 12 expert
 								)
 
 
@@ -293,28 +335,106 @@ colnames(res_combined)<-gsub("50.y","50.1",colnames(res_combined))
 res_combined$nLanIDs<-summary_095_min_bucs_obs_5$nLanIDs[match(res_combined$fisheryArea,summary_095_min_bucs_obs_5$fisheryArea)]
 res_combined
 
-res_0<-estimate_precision_from_sample_size(dat, proposed_sample_size0)$res_perc_sppWeight_nbucs_obs_5
-res_1<-estimate_precision_from_sample_size(dat, proposed_sample_size1)$res_perc_sppWeight_nbucs_obs_5
-
-res_combined<-merge(merge(res_0, proposed_sample_size0, by="fisheryArea"), 
-	merge(res_1, proposed_sample_size1, by="fisheryArea"), by="fisheryArea")
-colnames(res_combined)<-gsub("50.x","50.0",colnames(res_combined))
-colnames(res_combined)<-gsub("50.y","50.1",colnames(res_combined))
-res_combined$nLanIDs<-summary_095_min_bucs_obs_5$nLanIDs[match(res_combined$fisheryArea,summary_095_min_bucs_obs_5$fisheryArea)]
-res_combined
-
-
-
-
+#===============================================	
+# evaluation of proposed sample sizes (% of spp weight)
+#===============================================
+	# evaluates number of spp above 5% CV in present samples and expected under new sample size
+	
+# present situation
+dat[sppWeight_estim>0  & !is.na(sppWeight_estim_cv) & nbuc_obs>=5 & sp %in% c("SAN"), .N, .(lanID,sp, fisheryArea, sppWeight_estim_cv)][,list(sum(sppWeight_estim_cv>5)/.N), by=fisheryArea]
+dat[sppWeight_estim>0  & !is.na(sppWeight_estim_cv) & nbuc_obs>=5 & sp %in% c("SPR","HER"), .N, .(lanID,sp, fisheryArea, sppWeight_estim_cv)][,list(sum(sppWeight_estim_cv>5)/.N), by=fisheryArea]
+# proposed sampled size
+proposed_sample_size0<-summary_095_min_bucs_obs_5[nLanIDs>10,.(fisheryArea,n_0.050)]	
+estimate_precision_from_sample_size(dat, proposed_sample_size0)$dat[sppWeight_estim>0  & !is.na(pred_cv) & nbuc_obs>=5 & sp %in% c("SPR","HER"), .N, .(lanID,sp, fisheryArea, pred_cv)][,list(sum(pred_cv>.05)/.N), by=fisheryArea]
 
 
+#===============================================	
+# a look into the sand-eel
+#===============================================
+
+dat[sppWeight_estim>0 & sp %in% c("SAN") & fisheryArea=="NSea_SAN",.N,.(lanID, sp, nbuc_obs, totWeight_obs, sppPercWeight_estim=round(sppPercWeight_estim,2), sppWeight_estim=round(sppWeight_estim),sppWeight_estim_CIlow=round(sppWeight_estim_CIlow), sppWeight_estim_CIupp=round(sppWeight_estim_CIupp), sppWeight_estim_cv)][order(-sppWeight_estim),]
+
+
+dat[sppWeight_estim>0 & !sp %in% c("SAN") & fisheryArea=="NSea_SAN",.N,.(lanID, sp, nbuc_obs, totWeight_obs, sppPercWeight_estim=round(sppPercWeight_estim,2), sppWeight_estim=round(sppWeight_estim),sppWeight_estim_CIlow=round(sppWeight_estim_CIlow), sppWeight_estim_CIupp=round(sppWeight_estim_CIupp), sppWeight_estim_cv)][order(-sppWeight_estim),]
+head(dat[sppWeight_estim>0 & !sp %in% c("SAN") & fisheryArea=="NSea_SAN",.N,.(lanID, sp, nbuc_obs, totWeight_obs, sppPercWeight_estim=round(sppPercWeight_estim,2), sppWeight_estim=round(sppWeight_estim),sppWeight_estim_CIlow=round(sppWeight_estim_CIlow), sppWeight_estim_CIupp=round(sppWeight_estim_CIupp), sppWeight_estim_cv)][order(-sppWeight_estim),],20)
+tail(dat[sppWeight_estim>0 & !sp %in% c("SAN") & fisheryArea=="NSea_SAN",.N,.(lanID, sp, nbuc_obs, totWeight_obs, sppPercWeight_estim=round(sppPercWeight_estim,2), sppWeight_estim=round(sppWeight_estim),sppWeight_estim_CIlow=round(sppWeight_estim_CIlow), sppWeight_estim_CIupp=round(sppWeight_estim_CIupp), sppWeight_estim_cv)][order(-sppWeight_estim),],20)
 
 
 
+dat[sppWeight_estim>0 & sp %in% c("COD") & fisheryArea=="NSea_SAN",.N,.(lanID, sp, nbuc_obs, totWeight_obs, sppPercWeight_estim=round(sppPercWeight_estim,2), sppWeight_estim=round(sppWeight_estim),sppWeight_estim_CIlow=round(sppWeight_estim_CIlow), sppWeight_estim_CIupp=round(sppWeight_estim_CIupp), sppWeight_estim_cv)][order(-sppWeight_estim),]
+dat[sppWeight_estim>0 & sp %in% c("WHG") & fisheryArea=="NSea_SAN",.N,.(lanID, sp, nbuc_obs, totWeight_obs, sppPercWeight_estim=round(sppPercWeight_estim,2),sppWeight_estim=round(sppWeight_estim),sppWeight_estim_CIlow=round(sppWeight_estim_CIlow), sppWeight_estim_CIupp=round(sppWeight_estim_CIupp), sppWeight_estim_cv)][order(-sppWeight_estim),]
+dat[sppWeight_estim>0 & sp %in% c("HER") & fisheryArea=="NSea_SAN",.N,.(lanID, sp, nbuc_obs, totWeight_obs, sppPercWeight_estim=round(sppPercWeight_estim,2),sppWeight_estim=round(sppWeight_estim),sppWeight_estim_CIlow=round(sppWeight_estim_CIlow), sppWeight_estim_CIupp=round(sppWeight_estim_CIupp), sppWeight_estim_cv)][order(-sppWeight_estim),]
 
 
+#===============================================	
+# current evaluation of programmes
+#===============================================
+	# margin of error [% around total weight] vs sample size
+		targetFisheryArea<-"NSea_SAN"
+		data_graph<-dat[fisheryArea== targetFisheryArea & sppWeight_obs>0,.N,.(lanID,sp,sppPercWeight_estim_cv,sppWeight_estim_cv,percErrorMarginPercWeight=round(sppPercWeight_estim_errMargin/sppPercWeight_estim*100,1),percErrorMarginTotalWeight=round(sppWeight_estim_errMargin/sppWeight_estim*100,1))]
+		hist(data_graph$percErrorMarginTotalWeight, breaks=seq(0,300,by=1))
+		hist(data_graph[sp=="SPR",]$percErrorMarginTotalWeight, breaks=seq(0,300,by=1))
+		hist(data_graph[!sp=="SPR",]$percErrorMarginTotalWeight, breaks=seq(0,300,by=1))
+
+		targetFisheryArea<-"Baltic_HERSPR_HUC"
+		targetFisheryArea<-"Baltic_HERSPR_IND"
+		targetFisheryArea<-"Bothnia_FVE"
+		targetFisheryArea<-"Bothnia_HER"
+		targetFisheryArea<-"NAtlantic_MAC"
+		targetFisheryArea<-"NAtlantic_WHB"
+		targetFisheryArea<-"NSea_HER"
+		targetFisheryArea<-"NSea_NOP"
+		targetFisheryArea<-"NSea_SAN"
+		targetFisheryArea<-"NSea_SPR"
+		if(targetFisheryArea %in% c("Baltic_HERSPR_IND","Baltic_HERSPR_HUC")) targetSpp<-c("SPR","HER")
+		if(targetFisheryArea=="NSea_SPR") targetSpp<-c("SPR")
+		if(targetFisheryArea %in% c("Bothnia_HER", "NSea_HER")) targetSpp<-c("HER")
+		if(targetFisheryArea=="NSea_NOP") targetSpp<-c("NOP")
+		if(targetFisheryArea=="NSea_SAN") targetSpp<-c("SAN")
+		if(targetFisheryArea=="Bothnia_FVE") targetSpp<-c("FVE")
+		if(targetFisheryArea=="NAtlantic_MAC") targetSpp<-c("MAC")
+		if(targetFisheryArea=="NAtlantic_WHB") targetSpp<-c("WHB")
+		data_graph<-dat[fisheryArea== targetFisheryArea & sppWeight_obs>0 & nbuc_obs>=5,.N,.(lanID,sp,nbuc_obs,sppPercWeight_estim_cv,sppWeight_estim_cv,percErrorMarginPercWeight=round(sppPercWeight_estim_errMargin/sppPercWeight_estim*100,1),percErrorMarginTotalWeight=round(sppWeight_estim_errMargin/sppWeight_estim*100,1))]
+		windows(15,15); par(mfrow=c(3,1), oma=c(1,1,3,1))
+		ylimite=c(0, max(table(data_graph[,.N,.(lanID,nbuc_obs)]$nbuc_obs)))
+		if(!targetFisheryArea %in% c("NAtlantic_MAC","NAtlantic_WHB")) hist(data_graph[,.N,.(lanID,nbuc_obs)]$nbuc_obs, breaks=seq(0,60,by=1), main="No. Buckets Obs", xlab="n", ylim=ylimite)
+		if(targetFisheryArea %in% c("NAtlantic_MAC","NAtlantic_WHB")) hist(data_graph[,.N,.(lanID,nbuc_obs)]$nbuc_obs, breaks=seq(0,100,by=1), main="No. Buckets Obs", xlab="n", ylim=ylimite)
+		ylimite=c(0, max(table(cut(data_graph$percErrorMarginTotalWeight, breaks=seq(0,300,by=1), right = FALSE, ordered_result=T))))
+		hist(data_graph[sp %in% targetSpp,]$percErrorMarginTotalWeight, breaks=seq(0,300,by=1), main="error margin target spp", xlab="Percent Error Margin", ylim=ylimite, col="blue")
+		abline(v=5, lty=2, col="red")
+		hist(data_graph[!sp%in% targetSpp,]$percErrorMarginTotalWeight, breaks=seq(0,300,by=1), main=paste0("error margin bycatch\n",length(unique(data_graph[!sp%in% targetSpp,]$sp))," spp; mean ",round(data_graph[,.N,.(lanID,sp)][,!sp%in% targetSpp, .(lanID,sp)][,sum(V1),lanID][, mean(V1)],1),": spp per LanID"), xlab="Percent Error Margin", ylim=ylimite, col="brown")
+		abline(v=5, lty=2, col="red")
+		title(main=paste0(targetFisheryArea,": lanIDs (n>=5) = ",nrow(data_graph[,.N,.(lanID)])), outer=T, line=1, cex.main=1.5) 
 
 
+		#most sampled landings
+			# DNK_5019 - 91 buckets
+			dat[lanID=="DNK_5019",.N,.(lanID, sp, sppPercWeight_estim, sppPercWeight_estim_errMargin, sppWeight_estim, sppWeight_estim_errMargin)]
+			
+		
+#===============================================	
+# summing up
+#===============================================		
+	
+		targetFisheryArea<-"Baltic_HERSPR_IND"
+		targetFisheryArea<-"NSea_SPR"
+		targetFisheryArea<-"Baltic_HERSPR_HUC"
+		targetFisheryArea<-"NAtlantic_MAC"
+		targetFisheryArea<-"NSea_NOP"
+		if(targetFisheryArea %in% c("Baltic_HERSPR_IND","Baltic_HERSPR_HUC")) targetSpp<-c("SPR","HER")
+		if(targetFisheryArea=="NSea_SPR") targetSpp<-c("SPR")
+		if(targetFisheryArea %in% c("Bothnia_HER", "NSea_HER")) targetSpp<-c("HER")
+		if(targetFisheryArea=="NSea_NOP") targetSpp<-c("NOP")
+		if(targetFisheryArea=="NSea_SAN") targetSpp<-c("SAN")
+		if(targetFisheryArea=="Bothnia_FVE") targetSpp<-c("FVE")
+		if(targetFisheryArea=="NAtlantic_MAC") targetSpp<-c("MAC")		
+		data_graph<-dat[fisheryArea== targetFisheryArea & sppWeight_obs>0 & nbuc_obs>=2 & !sp %in% targetSpp,.N,.(year, lanID,sp,nbuc_obs,sppWeight_estim,sppWeight_estim_var)]
+		data_graph[, list(total=round(sum(sppWeight_estim)/1000,1), 
+						CIlow=round((sum(sppWeight_estim)-1.96*sqrt(sum(sppWeight_estim_var)))/1000,1),
+						CIupp=round((sum(sppWeight_estim)+1.96*sqrt(sum(sppWeight_estim_var)))/1000,1)
+						),.(year,sp)][order(-abs(total),sp, year),]
+
+		
 
 
 
